92d773e8ea413e5438ab7ca9801b1ac2987fcfa6
Pete Spangler <petertspangler@gmail.com>
2021-02-18 16:33:39 -0800

    Fun with Auth0

--- QandA/Startup.cs
+++ QandA/Startup.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,12 +11,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using DbUp;
 using QandA.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authorization;
+using QandA.Authorization;
 
 namespace QandA
 {
@@ -28,30 +32,54 @@ namespace QandA
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            var connectionString =
-                Configuration.GetConnectionString("DefaultConnection");
-
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
             EnsureDatabase.For.SqlDatabase(connectionString);
             var upgrader = DeployChanges.To
-                .SqlDatabase(connectionString, null)
-                .WithScriptsEmbeddedInAssembly(
-                    System.Reflection.Assembly.GetExecutingAssembly()
-                )
-                .WithTransaction()
-                .Build();
+                    .SqlDatabase(connectionString, null)
+                    .WithScriptsEmbeddedInAssembly(System.Reflection.Assembly.GetExecutingAssembly())
+                    .WithTransaction()
+                    .LogToConsole()
+                    .Build();
+
             if (upgrader.IsUpgradeRequired())
             {
                 upgrader.PerformUpgrade();
             }
+
             services.AddControllers();
-            services.AddScoped<IDataRepository,
-                DataRepository > ();
-            services.AddMemoryCache();
-            services.AddSingleton<IQuestionCache, QuestionCache>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "QandA", Version = "v1" });
             });
+
+            services.AddScoped<IDataRepository, DataRepository>();
+            services.AddMemoryCache();
+            services.AddSingleton<IQuestionCache, QuestionCache>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme =
+                  JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme =
+                  JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = Configuration["Auth0:Authority"];
+                options.Audience = Configuration["Auth0:Audience"];
+            });
+            services.AddHttpClient();
+            services.AddAuthorization(options =>
+                  options.AddPolicy("MustBeQuestionAuthor", policy =>
+                    policy.Requirements.Add(new MustBeQuestionAuthorRequirement())));
+            services.AddScoped<IAuthorizationHandler, MustBeQuestionAuthorHandler>();
+            services.AddHttpContextAccessor();
+
+            services.AddCors(options => options.AddPolicy("CorsPolicy", builder =>
+                  builder
+                   .AllowAnyMethod()
+                   .AllowAnyHeader()
+                   .WithOrigins(Configuration["Frontend"])));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -69,7 +97,8 @@ namespace QandA
             }
 
             app.UseRouting();
-
+            app.UseCors("CorsPolicy");
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
